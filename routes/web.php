bd51627c57d606750aab9367c3fccc584a8213ab
Dev <support@ilannoor.com>
2023-03-28 13:18:31 +0600

    # Laravel/Breeze Installed # npm Installed

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\WhyOurThemesController;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\HireController;
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\PrivacyPolicyController;
-use App\Http\Controllers\TermsOfServiceController;
-use App\Http\Controllers\SiteTemplatesController;
-use App\Http\Controllers\TemplateDetailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,34 +14,18 @@ use App\Http\Controllers\TemplateDetailController;
 |
 */
 
-// Home
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Why Our Themes
-Route::get('why-our-themes', [WhyOurThemesController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Blog
-Route::get('blog', [BlogController::class, 'index']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// About
-Route::get('about/overview', [AboutController::class, 'overview']);
-Route::get('about/brand', [AboutController::class, 'brand']);
-Route::get('about/license', [AboutController::class, 'license']);
-
-// Hire Us
-Route::get('hire-us', [HireController::class, 'index']);
-
-// Contact Us
-Route::get('contact-us', [ContactController::class, 'index']);
-
-// Privacy Policy
-Route::get('privacy-policy', [PrivacyPolicyController::class, 'index']);
-
-// Terms of Service
-Route::get('terms-of-service', [TermsOfServiceController::class, 'index']);
-
-// Site Templates
-Route::get('site-templates', [SiteTemplatesController::class, 'index']);
-
-// Template Detail
-Route::get('template-detail', [TemplateDetailController::class, 'index']);
+require __DIR__.'/auth.php';
